7c7be0097fc9e54a540df228ca0fe9bab03a6c2e
ABDERRAZZAK ELGHYATI <abderazakghiyati2@gmail.com>
2020-04-28 23:08:13 -0700

    Adding ejs template

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -17,7 +18,7 @@ const db = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
-    database:'products'
+    database:'stock'
 });
 
 // Check if database is connected ... !
@@ -29,7 +30,8 @@ db.connect(function(error){
 // Set views aka where to look for files
 app.set('views', 'views');
 
-app.get("/", (req, res) => {  
+// When User request home page
+app.get("/", (req, res) => {
     const sql = "SELECT * FROM ryon";
     const query = db.query(sql, (err, data) => {
         if(err) console.log(err);
@@ -43,15 +45,104 @@ app.get("/", (req, res) => {
 
 });
 
+
+// When request the product page of some ryon
+app.get("/ryon/:productId", (req, res) => {
+    // Get id from url
+    let productId = req.params.productId;
+
+    // Writing sql commands
+    const sql = "CREATE TABLE IF NOT EXISTS newProduct" + productId + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, image varchar(255), name varchar(255), quantity int(11) NOT NULL, price float NOT NULL) ;"
+
+    // Exeecute sql commands
+    const query = db.query(sql, (err, data) => {
+        if(err) throw err;
+        else{
+            db.query("SELECT * FROM newProduct" + productId, (err, data) => {
+                if (err) throw err;
+                res.render('addnewproducts', {
+                    products : data,
+                    productId: productId
+                });
+            });
+        }  
+        
+    });
+
+    // Show results
+    console.log("-- Request : ryon/" + productId);
+
+    // Writing to check file
+    fs.writeFileSync("check", productId);
+});
+
+// Creating New Products
+app.post("/newProduct/:productId", (req, res) => {
+    // Get Id From Url
+    let productId = req.params.productId;
+
+    // Get data from user
+    let data = {
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        quantity: req.body.quantity
+    };
+
+    // Check table exists
+    let sql = "CREATE TABLE IF NOT EXISTS newProduct" + productId + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, image varchar(255), name varchar(255), quantity int(11) NOT NULL, price float NOT NULL);";
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+        else{
+            db.query('INSERT INTO newProduct' + productId + ' SET ?', data, (err, result) => {
+                if (err) throw err;
+            });
+        }
+      });
+      res.end("Product added successfully !");
+
+    // Show results
+    console.log("-- Request : newProduct/" + productId);
+      
+});
+
+// Editing New Products
+app.get('/editProduct/:productId',(req, res) => {
+    let productId = req.params.productId;
+
+    // Reading from file
+    let rf = fs.readFileSync("check", "utf8");
+
+    // Rendering editProducts Page
+    res.render("editProducts", {
+        productId : req.params.productId
+    });
+
+    app.post('/editProduct/' + productId,(req, res) => {
+        let sql = `Update newProduct` + rf + ` SET image='${req.body.image}', name='${req.body.name}', quantity='${req.body.quantity}', price='${req.body.price}' where id=${productId}`;
+        let query = db.query(sql, (err, results) => {
+          if(err) throw err;
+        });
+        res.end("Product edited successfully !");
+    });
+
+    // console.log("request : /editProducts : | " + productId);
+    console.log("Updating table newProduct" + rf);
+    console.log("Requesting /editProduct" + productId);
+    console.log("This will execute Update table newProduct" + rf + " where id=" + productId);
+});
+
+// When request electroProduct Static Page
 app.get("/electroProducts", (req, res) => {
     res.render("eclectroProducts", {})
 });
 
+// When request fruitProducts Static Page
 app.get("/fruitProducts", (req, res) => {
     res.render("fruitProducts", {})
 });
 
-// When user trying to create a new quote
+// Create table for each newly created ryon
 app.post("/newRyon", (req, res) => {
     // Get data from user
     let data = {
@@ -59,41 +150,81 @@ app.post("/newRyon", (req, res) => {
         image: req.body.image
     };
 
-    
-
+    // Get number of created tables
     db.query('INSERT INTO ryon SET ?', data, function (err, result) {
       if (err) throw err;
       res.redirect('/');
     });
 
-    // res.end("Data Created Successfuly ... !")
+    // Creating check file for reading later new product
+    if (fs.existsSync("check")){
+        // Reading from check file
+        let rf = fs.readFileSync("check", 'utf8');
+
+        // Increment check file value by 1
+        fs.writeFileSync("check", rf++);
+    }else{
+        fs.writeFileSync("check", 0);
+    }
+
 });
 
-// QUOTE Editing 
-app.get('/edit/:cardId',(req, res) => {
-    const cardId = req.params.cardId;
+// Quote new products
+app.get("/delete/:productId", (req, res) => {
+    const productId = req.params.productId;
+    // Reading from file
+    let rf = fs.readFileSync("check", "utf8");
 
-    res.render("editQuote", {
-        cardId : req.params.cardId
-    });
-
-    app.post('/edit/' + cardId,(req, res) => {
-        let sql = `Update cards SET pictureUrl='../img/${req.body.pictureUrl}', name='${req.body.name}', description='${req.body.description}' where ID=${cardId}`;
-        let query = db.query(sql, (err, results) => {
-          if(err) throw err;
-          res.redirect('/');
-        });
-    });
-});
-
-// Quote Deleting
-app.get("/delete/:cardId", (req, res) => {
-    const cardId = req.params.cardId;
-    let sql = `DELETE FROM cards where ID=${cardId}`;
-    let query = db.query(sql, (err, results) => {
+    let sql = `DELETE FROM newProduct` + rf + ` where ID=${productId}`;
+    db.query(sql, (err, results) => {
       if(err) throw err;
       res.redirect('/');
     });
+
+    // Show results
+    console.log("Deleting newProduct" + rf + " where id=" + productId);
+});
+
+// Deleting ryon from home
+app.get("/deleteRyon/:ryonId", (req, res) => {
+    const ryonId = req.params.ryonId;
+    // Reading from file
+    let rf = fs.readFileSync("check", "utf8");
+
+    let sql = `DELETE FROM ryon where ID=${ryonId}`;
+    db.query(sql, (err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+
+    // Show results
+    console.log("Deleting from table ryon where id=" + ryonId);
+});
+
+// Editing Ryon From Home
+app.get('/editRyon/:productId',(req, res) => {
+    let productId = req.params.productId;
+
+    // Reading from file
+    let rf = fs.readFileSync("check", "utf8");
+
+    // Rendering editProducts Page
+    res.render("editingRyon", {
+        productId : req.params.productId
+    });
+
+    app.post('/editRyon/' + productId,(req, res) => {
+        let sql = `Update ryon SET image='${req.body.image}', name='${req.body.name}' where id=${productId}`;
+        db.query(sql, (err, results) => {
+          if(err) throw err;
+        });
+        res.end("Ryon edited successfully !");
+    });
+
+    // console.log("request : /editProducts : | " + productId);
+    // console.log("Updating table newProduct" + rf);
+    console.log("Requesting /editRyon" + productId);
+    console.log("This will execute Update table newProduct" + rf + " where id=" + productId);
 });
 
 //  Listing Server 
